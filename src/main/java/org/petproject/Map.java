20eef14d5013a006008ed2b548a8c4a9e9c4562d
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-01 16:04:23 +0700

    feat: add `initMap` method in Map class

--- src/main/java/org/petproject/Map.java
+++ src/main/java/org/petproject/Map.java
@@ -1,4 +1,22 @@
 package org.petproject;
 
+import org.petproject.entity.Entity;
+import org.petproject.entity.Ground;
+
+import java.util.Arrays;
+
 public class Map {
+    private Entity[][] map;
+
+    public void initMap(int x, int y) {
+        this.map = new Entity[x][y];
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                map[i][j] = new Ground(new Coordinates(i, j));
+            }
+        }
+
+        System.out.println(Arrays.deepToString(map));
+    }
 }
