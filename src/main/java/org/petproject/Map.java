59b57ea9dcfbf9fee44f50ca475d35f64aea3c9f
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-02 16:10:56 +0700

    feat: attempt to implement logic of `Map` class

--- src/main/java/org/petproject/Map.java
+++ src/main/java/org/petproject/Map.java
@@ -1,14 +1,20 @@
 package org.petproject;
 
-import org.petproject.entity.Entity;
-import org.petproject.entity.Ground;
+import org.petproject.creature.Herbivore;
+import org.petproject.creature.Predator;
+import org.petproject.entity.*;
 
-import java.util.Arrays;
+import java.util.Random;
 
 public class Map {
     private Entity[][] map;
 
-    public void initMap(int x, int y) {
+    /**
+     * Create a map of given x and y size, filled with Ground objects
+     * @param x - horizontal size of a map
+     * @param y - vertical size of a map
+     */
+    public void initEmptyMap(int x, int y) {
         this.map = new Entity[x][y];
 
         for (int i = 0; i < map.length; i++) {
@@ -16,7 +22,74 @@ public class Map {
                 map[i][j] = new Ground(new Coordinates(i, j));
             }
         }
+    }
 
-        System.out.println(Arrays.deepToString(map));
+    /**
+     * Fill given map with all the available in simulation objects in random way
+     * @param map to fill with objects
+     * @return map filled with objects
+     */
+    public Entity[][] fillMapWithRandomObjects(Entity[][] map) {
+        int[] objectsPool = createPoolOfObjectsForMap(map);
+
+        for (int i = 0; i < objectsPool.length; i++) {
+            for (int j = 0; j < objectsPool[i]; j++) {
+                Entity entity = null;
+
+                if (i == 0) {
+                    entity = new Grass();
+                }
+                if (i == 1) {
+                    entity = new Herbivore();
+                }
+                if (i == 2) {
+                    entity = new Predator();
+                }
+                if (i == 3) {
+                    entity = new Tree();
+                }
+                if (i == 4) {
+                    entity = new Rock();
+                }
+
+                Coordinates randomCoordinates = randomizeCoordinatesOfObject(entity);
+            }
+        }
+
+
+        return map;
+    }
+
+    /**
+     * Create a pool of the objects that should be placed into the map
+     * @param map for which pool should be created
+     * @return array of ints in which each of the number represent
+     * amount of objects that should be created in the following order -
+     * Grass, Herbivore, Predator, Tree, Rock
+     */
+    private int[] createPoolOfObjectsForMap(Entity[][] map) {
+        int x = map.length;
+        int y = map[0].length;
+        int mapArea = x * y;
+
+        int amountOfGrass = mapArea / 2;
+        int amountOfHerbivores = amountOfGrass / 10;
+        int amountOfPredators = amountOfHerbivores / 10;
+        int amountOfTress = amountOfGrass / 10;
+        int amountOfRocks = amountOfGrass / 10;
+
+        return new int[]{amountOfGrass, amountOfHerbivores, amountOfPredators, amountOfTress, amountOfRocks};
+    }
+
+    private Coordinates randomizeCoordinatesOfObject(Entity entity) {
+        int xUpperBound = map.length;
+        int yUpperBound = map[0].length;
+
+        Random random = new Random();
+
+        int randomX = random.nextInt(xUpperBound);
+        int randomY = random.nextInt(yUpperBound);
+
+        return new Coordinates(randomX, randomY);
     }
 }
