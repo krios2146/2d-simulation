d4e70a07a8e554d69dea4814efad689a65f96ca4
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-03 10:17:20 +0700

    feat: add getters & setters for Coordinates field in Entity class

--- src/main/java/org/petproject/entity/Entity.java
+++ src/main/java/org/petproject/entity/Entity.java
@@ -4,4 +4,12 @@ import org.petproject.Coordinates;
 
 public abstract class Entity {
     private Coordinates coordinates;
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
