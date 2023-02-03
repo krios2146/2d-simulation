107934a082e146dd2c724464a409890bf115ef83
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-03 14:03:49 +0700

    feat: add `processCreatureMoves` method to `TurnAction` class

--- src/main/java/org/petproject/action/TurnAction.java
+++ src/main/java/org/petproject/action/TurnAction.java
@@ -1,5 +1,24 @@
 package org.petproject.action;
 
+import org.petproject.Map;
+import org.petproject.entity.Entity;
+import org.petproject.entity.creature.Creature;
+
 public class TurnAction extends Action {
 
+    /**
+     Processes the moves of all creatures on the map.
+     @param mapObj The map object which contains all the entities, including the creatures.
+     */
+    public void processCreatureMoves(Map mapObj) {
+        Entity[][] map = mapObj.getMap();
+
+        for (Entity[] entities : map) {
+            for (Entity entity : entities) {
+                if (entity instanceof Creature creature) {
+                    creature.makeMove();
+                }
+            }
+        }
+    }
 }
