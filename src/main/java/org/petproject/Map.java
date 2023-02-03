35bcff6962aa8b62e16cf89601f972735e81e2c1
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-03 13:20:28 +0700

    feat: move logic associated with initiation of the map from the `Map` class to `InitAction` class

--- src/main/java/org/petproject/Map.java
+++ src/main/java/org/petproject/Map.java
@@ -1,102 +1,42 @@
 package org.petproject;
 
-import org.petproject.creature.Herbivore;
-import org.petproject.creature.Predator;
-import org.petproject.entity.*;
-
-import java.util.Random;
+import org.petproject.entity.Entity;
 
 public class Map {
-    private Entity[][] map;
+    int x;
+    int y;
 
-    /**
-     * Create a map of given x and y size, filled with Ground objects
-     * @param x - horizontal size of a map
-     * @param y - vertical size of a map
-     */
-    public void initEmptyMap(int x, int y) {
-        this.map = new Entity[x][y];
+    private Entity[][] map = new Entity[x][y];
 
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                map[i][j] = new Ground(new Coordinates(i, j));
-            }
-        }
+    public Map(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    /**
-     * Fill given map with all the available in simulation objects in random way
-     * @param map to fill with objects
-     * @return map filled with objects
-     */
-    public Entity[][] fillMapWithRandomObjects(Entity[][] map) {
-        int[] objectsPool = createPoolOfObjectsForMap(map);
+    public Map() {
+        this.x = 10;
+        this.y = 10;
+    }
 
-        for (int i = 0; i < objectsPool.length; i++) {
-            for (int j = 0; j < objectsPool[i]; j++) {
-                Entity entity = null;
-
-                if (i == 0) {
-                    entity = new Grass();
-                }
-                if (i == 1) {
-                    entity = new Herbivore();
-                }
-                if (i == 2) {
-                    entity = new Predator();
-                }
-                if (i == 3) {
-                    entity = new Tree();
-                }
-                if (i == 4) {
-                    entity = new Rock();
-                }
-
-                Coordinates randomCoordinates = randomizeCoordinatesOfObject(entity);
-                entity.setCoordinates(randomCoordinates);
-
-                addObject(entity);
-            }
-        }
-
+    public Entity[][] getMap() {
         return map;
     }
 
+    public void setMap(Entity[][] map) {
+        this.map = map;
+    }
+
+
+    /**
+     * Adds an object to the map.
+     * <p>
+     * This method takes an `Entity` object and adds it to the map at the coordinates specified by the object's `Coordinates`. The x and y values of the `Coordinates` are used to determine the position in the map where the object will be added.
+     *
+     * @param object The `Entity` object to be added to the map.
+     */
     public void addObject(Entity object) {
         Coordinates coordinates = object.getCoordinates();
         map[coordinates.x][coordinates.y] = object;
     }
 
-    /**
-     * Create a pool of the objects that should be placed into the map
-     * @param map for which pool should be created
-     * @return array of ints in which each of the number represent
-     * amount of objects that should be created in the following order -
-     * Grass, Herbivore, Predator, Tree, Rock
-     */
-    private int[] createPoolOfObjectsForMap(Entity[][] map) {
-        int x = map.length;
-        int y = map[0].length;
-        int mapArea = x * y;
-
-        int amountOfGrass = mapArea / 2;
-        int amountOfHerbivores = amountOfGrass / 10;
-        int amountOfPredators = amountOfHerbivores / 10;
-        int amountOfTress = amountOfGrass / 10;
-        int amountOfRocks = amountOfGrass / 10;
-
-        return new int[]{amountOfGrass, amountOfHerbivores, amountOfPredators, amountOfTress, amountOfRocks};
-    }
-
-    private Coordinates randomizeCoordinatesOfObject(Entity entity) {
-        int xUpperBound = map.length;
-        int yUpperBound = map[0].length;
-
-        Random random = new Random();
-
-        int randomX = random.nextInt(xUpperBound);
-        int randomY = random.nextInt(yUpperBound);
-
-        return new Coordinates(randomX, randomY);
-    }
 }
--- src/main/java/org/petproject/action/InitAction.java
+++ src/main/java/org/petproject/action/InitAction.java
@@ -1,5 +1,89 @@
 package org.petproject.action;
 
+import org.petproject.Coordinates;
+import org.petproject.Map;
+import org.petproject.entity.Entity;
+import org.petproject.entity.Grass;
+import org.petproject.entity.Rock;
+import org.petproject.entity.Tree;
+import org.petproject.entity.creature.Herbivore;
+import org.petproject.entity.creature.Predator;
+
+import java.util.HashMap;
+import java.util.Random;
+
 public class InitAction extends Action {
 
+    private Map map;
+
+    public Map initMap() {
+        map = new Map();
+
+        HashMap<Entity, Integer> pool = createObjectPoolForMap(map);
+
+        pool.forEach((entity, amountOfEntity) -> {
+            for (int i = 0; i < amountOfEntity; i++) {
+                Entity entityWithRandomCoordinates = assignRandomCoordinatesToEntity(entity);
+                map.addObject(entityWithRandomCoordinates);
+            }
+        });
+
+        return map;
+    }
+
+    /**
+     * Creates a pool of objects for a given map.
+     * <p>
+     * This method uses the size of the map to calculate the total area and the amount of each type of object (Grass, Herbivore, Predator, Tree, Rock) that should be created. These amounts are then stored as key-value pairs in a `HashMap`, with the object as the key and the amount as the value. The resulting `HashMap` is returned.
+     *
+     * @param map The map for which the object pool is being created.
+     * @return A `HashMap` containing the objects and their corresponding amounts.
+     */
+    private HashMap<Entity, Integer> createObjectPoolForMap(Map map) {
+        HashMap<Entity, Integer> pool = new HashMap<>();
+
+        int x = map.getMap().length;
+        int y = map.getMap()[0].length;
+        int mapArea = x * y;
+
+        int amountOfGrass = mapArea / 2;
+        pool.put(new Grass(), amountOfGrass);
+
+        int amountOfHerbivores = amountOfGrass / 10;
+        pool.put(new Herbivore(), amountOfHerbivores);
+
+        int amountOfPredators = amountOfHerbivores / 10;
+        pool.put(new Predator(), amountOfPredators);
+
+        int amountOfTress = amountOfGrass / 10;
+        pool.put(new Tree(), amountOfTress);
+
+        int amountOfRocks = amountOfGrass / 10;
+        pool.put(new Rock(), amountOfRocks);
+
+        return pool;
+    }
+
+    /**
+     * Assigns random coordinates to a given entity.
+     * <p>
+     * This method uses the length and width of the map to determine the upper bounds for the x and y coordinates. A `java.util.Random` object is used to generate random values within these bounds. The resulting x and y coordinates are used to create a `Coordinates` object, which is then set as the entity's coordinates. The entity with its updated coordinates is returned.
+     *
+     * @param entity The entity for which the random coordinates are being assigned.
+     * @return The same entity with updated `Coordinates` containing randomly generated x and y values within the bounds of the map.
+     */
+    private Entity assignRandomCoordinatesToEntity(Entity entity) {
+        int xUpperBound = map.getMap().length;
+        int yUpperBound = map.getMap()[0].length;
+
+        Random random = new Random();
+
+        int randomX = random.nextInt(xUpperBound);
+        int randomY = random.nextInt(yUpperBound);
+
+        Coordinates coordinates = new Coordinates(randomX, randomY);
+
+        entity.setCoordinates(coordinates);
+        return entity;
+    }
 }
