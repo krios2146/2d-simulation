af2f83f7b53be9ba203987b61aa20a32677a8c53
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-06 18:11:14 +0700

    feat: update `Coordinates` class, now x & y, can pe accessed only via getters and setters

--- src/main/java/org/petproject/Coordinates.java
+++ src/main/java/org/petproject/Coordinates.java
@@ -1,7 +1,7 @@
 package org.petproject;
 
 public class Coordinates {
-    int x, y;
+    private int x, y;
 
     public Coordinates(int x, int y) {
         this.x = x;
@@ -9,5 +9,21 @@ public class Coordinates {
     }
 
     public Coordinates() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
