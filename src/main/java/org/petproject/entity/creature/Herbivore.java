9e683520c31c4eae76b31ec61063ba8a80362767
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-06 11:02:38 +0700

    feat: add first implementation of `makeMove()` to Creatures

--- src/main/java/org/petproject/entity/creature/Herbivore.java
+++ src/main/java/org/petproject/entity/creature/Herbivore.java
@@ -1,14 +1,39 @@
 package org.petproject.entity.creature;
 
+import org.petproject.BreadthFirstSearch;
+import org.petproject.Coordinates;
+import org.petproject.entity.Grass;
+
+import java.util.List;
+
 public class Herbivore extends Creature {
+    private final BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
 
     @Override
     public void makeMove() {
-
+        List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Grass());
+        Coordinates coordinatesToMove = wayToObject.get(speed - 1);
+        this.setCoordinates(coordinatesToMove);
     }
 
     @Override
     public String toString() {
         return "H";
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
--- src/main/java/org/petproject/entity/creature/Predator.java
+++ src/main/java/org/petproject/entity/creature/Predator.java
@@ -1,11 +1,20 @@
 package org.petproject.entity.creature;
 
+import org.petproject.BreadthFirstSearch;
+import org.petproject.Coordinates;
+import org.petproject.entity.Grass;
+
+import java.util.List;
+
 public class Predator extends Creature {
     int attack;
+    private final BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
 
     @Override
     public void makeMove() {
-
+        List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Grass());
+        Coordinates coordinatesToMove = wayToObject.get(speed - 1);
+        this.setCoordinates(coordinatesToMove);
     }
 
     @Override
