48b8f2523cbd370b1dc97881c79fb9bbe000efac
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-06 18:16:52 +0700

    feat: now `Herbivore` can eat `Grass` if it's close enough

--- src/main/java/org/petproject/entity/creature/Herbivore.java
+++ src/main/java/org/petproject/entity/creature/Herbivore.java
@@ -2,9 +2,13 @@ package org.petproject.entity.creature;
 
 import org.petproject.BreadthFirstSearch;
 import org.petproject.Coordinates;
+import org.petproject.entity.Entity;
 import org.petproject.entity.Grass;
+import org.petproject.entity.Ground;
 
 import java.util.List;
+
+import static org.petproject.Main.simulation;
 
 public class Herbivore extends Creature {
     private final BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
@@ -12,8 +16,16 @@ public class Herbivore extends Creature {
     @Override
     public void makeMove() {
         List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Grass());
-        Coordinates coordinatesToMove = wayToObject.get(speed - 1);
-        this.setCoordinates(coordinatesToMove);
+
+        if (wayToObject.size() == 1) {
+            Coordinates coordinatesOfGrass = wayToObject.get(0);
+            Entity[][] map = simulation.getMap().getMap();
+            map[coordinatesOfGrass.getX()][coordinatesOfGrass.getY()] = new Ground(coordinatesOfGrass);
+        }
+        else {
+            Coordinates coordinatesToMove = wayToObject.get(speed - 1);
+            setCoordinates(coordinatesToMove);
+        }
     }
 
     @Override
