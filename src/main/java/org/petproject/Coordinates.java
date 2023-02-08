bb9eed956deecfa1fdd9a4c8284082df8c029727
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-08 14:57:20 +0700

    fix: override equals & hashCode for `Coordinates`

--- src/main/java/org/petproject/Coordinates.java
+++ src/main/java/org/petproject/Coordinates.java
@@ -26,4 +26,22 @@ public class Coordinates {
     public void setY(int y) {
         this.y = y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+
+        if (x != that.x) return false;
+        return y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
