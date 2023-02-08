aea2f587ea2ebc0cd9ce34dcea4122481b65e5dd
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-08 12:36:30 +0700

    feat: add implementation of simple console `Renderer`

--- src/main/java/org/petproject/Renderer.java
+++ src/main/java/org/petproject/Renderer.java
@@ -1,4 +1,16 @@
 package org.petproject;
 
+import org.petproject.entity.Entity;
+
 public class Renderer {
+    public void renderMap(Map mapObj) {
+        Entity[][] map = mapObj.getMap();
+
+        for (Entity[] entityRow : map) {
+            for (Entity entity : entityRow) {
+                System.out.print(entity.toString() + " ");
+            }
+            System.out.println();
+        }
+    }
 }
