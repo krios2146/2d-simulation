6c0fb3511460b98773acefbd95173860109f8cf4
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-08 14:58:03 +0700

    feat: implement methods in `Simulation` class

--- src/main/java/org/petproject/Simulation.java
+++ src/main/java/org/petproject/Simulation.java
@@ -3,25 +3,30 @@ package org.petproject;
 import org.petproject.action.InitAction;
 import org.petproject.action.TurnAction;
 
+import java.util.concurrent.TimeUnit;
+
 public class Simulation {
     private Map map;
     private final InitAction initAction = new InitAction();
     private final TurnAction turnAction = new TurnAction();
-    private Renderer renderer;
+    private final Renderer renderer = new Renderer();
     private int movesCounter = 0;
     private boolean isGameStopped = false;
 
     public void nextTurn() {
         turnAction.processCreatureMoves(map);
+        turnAction.renderMap(map);
+        System.out.println(movesCounter);
     }
 
-    public void startSimulation() {
+    public void startSimulation() throws InterruptedException {
         map = initAction.initMap();
+        turnAction.renderMap(map);
 
         while (!isGameStopped) {
             nextTurn();
-
             movesCounter++;
+            TimeUnit.SECONDS.sleep(1);
         }
     }
 
@@ -31,5 +36,9 @@ public class Simulation {
 
     public Map getMap() {
         return map;
+    }
+
+    public Renderer getRenderer() {
+        return renderer;
     }
 }
