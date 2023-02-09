c26bcee72560d784528447e451d4b6c248f07f83
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-09 13:30:18 +0700

    chore: split logic of `Herbivore` to two private methods

--- src/main/java/org/petproject/entity/creature/Herbivore.java
+++ src/main/java/org/petproject/entity/creature/Herbivore.java
@@ -11,21 +11,34 @@ import java.util.List;
 import static org.petproject.Main.simulation;
 
 public class Herbivore extends Creature {
-    private final BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
+    private BreadthFirstSearch breadthFirstSearch;
 
     @Override
     public void makeMove() {
+        breadthFirstSearch = new BreadthFirstSearch();
         List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Grass());
 
         if (wayToObject.size() == 1) {
-            Coordinates coordinatesOfGrass = wayToObject.get(0);
-            Entity[][] map = simulation.getMap().getMap();
-            map[coordinatesOfGrass.getX()][coordinatesOfGrass.getY()] = new Ground(coordinatesOfGrass);
+            eatGrass(wayToObject);
         }
         else {
-            Coordinates coordinatesToMove = wayToObject.get(speed - 1);
-            setCoordinates(coordinatesToMove);
+            moveToGrass(wayToObject);
         }
+    }
+
+    private void moveToGrass(List<Coordinates> wayToObject) {
+        // Remove herbivore from it current coordinates
+        Entity[][] map = simulation.getMap().getMap();
+        map[coordinates.getX()][coordinates.getY()] = new Ground(new Coordinates(coordinates.getX(), coordinates.getY()));
+        // Set new coordinates to herbivore
+        Coordinates coordinatesToMove = wayToObject.get(speed - 1);
+        setCoordinates(coordinatesToMove);
+    }
+
+    private static void eatGrass(List<Coordinates> wayToObject) {
+        Coordinates coordinatesOfGrass = wayToObject.get(0);
+        Entity[][] map = simulation.getMap().getMap();
+        map[coordinatesOfGrass.getX()][coordinatesOfGrass.getY()] = new Ground(coordinatesOfGrass);
     }
 
     @Override
